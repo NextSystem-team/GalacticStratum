a1bea9f1e06fd47a069203b0dda3761e0171d84a
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-27 11:56:31 -0300

    Iniciando implementação de movimento

--- GalacticStratum/Assets/_GalacticStratum/Scripts/Player/Player.cs
+++ GalacticStratum/Assets/_GalacticStratum/Scripts/Player/Player.cs
@@ -4,25 +4,49 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float secondsToReachMaxSpeed;
 
     private Vector2 movement;
+    private Vector2 currentVelocity;
 
     [SerializeField] private InputActionReference move;
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         HandleActions();
+    }
+
+    private void FixedUpdate()
+    {
         ApplyMovement();
     }
 
     private void HandleActions()
     {
-        movement = move.action.ReadValue<Vector2>().normalized * movementSpeed * Time.deltaTime;
+        Vector2 moveValue = move.action.ReadValue<Vector2>().normalized * maxSpeed;
+        movement = Vector2.SmoothDamp(movement, moveValue, ref currentVelocity, (maxSpeed/secondsToReachMaxSpeed));
+
+        if (Vector2.Distance(movement, moveValue) <= 1.0f)
+        {
+            movement = moveValue;
+        }
+
+        if (movement == Vector2.zero)
+        {
+            currentVelocity = Vector2.zero;
+        }
     }
 
     private void ApplyMovement()
     {
-        transform.Translate(new Vector3(movement.x, movement.y, 0.0f));
+        rigidBody.linearVelocity = movement;
     }
 }
