0c724d06c10d39ef1539f46117e8e54347c1da32
Lohannes da Silva Costa <lohannes.s26@gmail.com>
2026-05-28 10:03:35 -0300

    Implementada movimentação

--- GalacticStratum/Assets/_GalacticStratum/Scripts/Player/Player.cs
+++ GalacticStratum/Assets/_GalacticStratum/Scripts/Player/Player.cs
@@ -4,11 +4,15 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float maxSpeed;
     [SerializeField] private float secondsToReachMaxSpeed;
+    [SerializeField] private float secondsToStop;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField, Range(0f, 1f)] private float driftFactor; //O quanto vai deslizar nas curvas
 
     private Vector2 movement;
-    private Vector2 currentVelocity;
+    private Vector2 moveInputValue;
 
     [SerializeField] private InputActionReference move;
 
@@ -27,26 +31,47 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovement();
+        ApplyRotation();
     }
 
     private void HandleActions()
     {
-        Vector2 moveValue = move.action.ReadValue<Vector2>().normalized * maxSpeed;
-        movement = Vector2.SmoothDamp(movement, moveValue, ref currentVelocity, (maxSpeed/secondsToReachMaxSpeed));
+        moveInputValue = move.action.ReadValue<Vector2>();
+    }
 
-        if (Vector2.Distance(movement, moveValue) <= 1.0f)
-        {
-            movement = moveValue;
-        }
-
-        if (movement == Vector2.zero)
-        {
-            currentVelocity = Vector2.zero;
-        }
+    private void FixDrift()
+    {
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(movement, transform.up); //Coleta a velocidade vertical
+        Vector2 rightVelocity = transform.right * Vector2.Dot(movement, transform.right); //Coleta a velocidade horizontal
+        movement = forwardVelocity + (rightVelocity * driftFactor); 
+        //Reduz a velocidade horizontal, fazendo o player deslizar menos ou mais nas curvas
     }
 
     private void ApplyMovement()
     {
+        FixDrift();
+
+        Vector2 targetVelocity = moveInputValue.y > 0 ? (Vector2)transform.up * maxSpeed : Vector2.zero; //Se o input for positivo, o targetVelocity é para frente
+
+        float timeToReachTargetSpeed = moveInputValue.y == 0 ? secondsToStop : secondsToReachMaxSpeed;
+
+        movement = Vector2.MoveTowards(movement, targetVelocity, (maxSpeed/timeToReachTargetSpeed) * Time.fixedDeltaTime);
+
+        if (Vector2.Distance(movement, targetVelocity) <= 0.5f)
+        {
+            movement = targetVelocity;
+        }
+
         rigidBody.linearVelocity = movement;
+    }
+
+    private void ApplyRotation()
+    {
+        if (moveInputValue.x != 0)
+        {
+            float targetAngle = -moveInputValue.x * rotationSpeed * Time.fixedDeltaTime;
+
+            rigidBody.MoveRotation(rigidBody.rotation + targetAngle);
+        }
     }
 }
